284163d595e38a1dcce1d2b07ffdad65f8aac4f1
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2024-11-25 21:35:58 +0900

    [Gold V] Title: 토마토, Time: 3500 ms, Memory: 50468 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,15 +1,9 @@
 from collections import deque
 
 m,n,h = map(int,input().split())
-graph = [[] for _ in range(h)]
+graph = [[list(map(int,input().split())) for _ in range(n)] for _ in range(h)]
 result = []
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-dbox = [1,-1]
-
-for box in range(h):
-    for _ in range(n):
-        graph[box].append(list(map(int,input().split())))
+d = [(-1,0,0),(1,0,0),(0,-1,0),(0,1,0),(0,0,-1),(0,0,1)]
 
 queue = deque()
 
@@ -17,29 +11,23 @@ for box in range(h):
     for x in range(n):
         for y in range(m):
             if graph[box][x][y]==1:
-                queue.append([box,x,y,1])
+                queue.append((box,x,y,1))
 
 result = 1
 
 while queue:
     box, x, y, date = queue.popleft()
     result = date
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+    for dbox, dx, dy in d:
+        nbox = box + dbox
+        nx = x + dx
+        ny = y + dy
+        if nx < 0 or nx >= n or ny < 0 or ny >= m or nbox < 0 or nbox >= h:
             continue
-        if graph[box][nx][ny] == 0:
-            graph[box][nx][ny] = date+1
-            queue.append([box,nx,ny,date+1])
-    for i in range(2):
-        nbox = box + dbox[i]
-        if nbox < 0 or nbox >= h:
-            continue
-        if graph[nbox][x][y] == 0:
-            graph[nbox][x][y] = date+1
-            queue.append([nbox,x,y,date+1])
-
+        if graph[nbox][nx][ny] == 0:
+            graph[nbox][nx][ny] = date+1
+            queue.append((nbox,nx,ny,date+1))
+            
 success = True
 
 for box in range(h):
