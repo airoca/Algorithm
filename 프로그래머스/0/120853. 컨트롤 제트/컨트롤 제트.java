0842a26bdfffa897dd7a217c73f07beb4c628306
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-01-31 02:12:22 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.35 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,6 +1,23 @@
+import java.util.Stack;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for (String digit : s.split(" ")) {
+            if (digit.equals("Z")) {
+                stack.pop();
+            }
+            else {
+                stack.push(Integer.parseInt(digit));
+            }
+        }
+        
+        for (int i : stack) {
+            answer += i;
+        }
+        
         return answer;
     }
 }
