3e13d70f51059e6467093f33cc9d3a5879e19ee4
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-02-17 17:53:37 +0900

    [Platinum III] Title: 강수량, Time: 368 ms, Memory: 36908 KB -BaekjoonHub

--- 백준/Platinum/2094. 강수량/강수량.java
+++ 백준/Platinum/2094. 강수량/강수량.java
@@ -2,25 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	public static BufferedReader br;
-	public static BufferedWriter bw;
-	
-    public static int n;
-    public static int m;
-    public static int base;
-    public static int[] indexTree;
-    public static int[] years;
-    public static HashMap<Integer, Integer> map;
+    
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+    
+    static int n, m, base;
+    static int[] indexTree;
+    static HashMap<Integer, Integer> map;
+    static int[] years;
 
     public static void main(String[] args) throws IOException {
-
+        
         br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         n = Integer.parseInt(br.readLine());
-        base = 1;
+        map = new HashMap<Integer, Integer>();
         
+        base = 1;
         while (base < n) {
         	base <<= 1;
         }
@@ -28,66 +28,67 @@ public class Main {
         indexTree = new int[base * 2];
         years = new int[n+1];
         
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        // 인덱스 트리 생성, 연도 연결 정보 기록
-        for (int i = 1; i < (n+1); i++) {
-        	StringTokenizer st = new StringTokenizer(br.readLine());
-        	int year = Integer.parseInt(st.nextToken());
-        	int rain = Integer.parseInt(st.nextToken());
-        	
-        	years[i] = year;
-        	map.put(year, i);
-        	
-        	update(i, rain);
+        for (int i = 1; i <= n; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int y = Integer.parseInt(st.nextToken());
+        	int r = Integer.parseInt(st.nextToken());
+        	map.put(y, i);
+        	update(i,r);
+        	years[i] = y;
         }
         
         m = Integer.parseInt(br.readLine());
         
-        // 사람들의 이야기 입력, 쿼리 진행
+        int y, x, left, right, maxValue;
         for (int i = 0; i < m; i++) {
-        	StringTokenizer st = new StringTokenizer(br.readLine());
-        	int yearY = Integer.parseInt(st.nextToken());
-        	int yearX = Integer.parseInt(st.nextToken());
+        	st = new StringTokenizer(br.readLine());
+        	y = Integer.parseInt(st.nextToken());
+        	x = Integer.parseInt(st.nextToken());
         	
-        	// 1. Y와 X 정보 모두 없다면 항상 maybe 출력
-        	if (!map.containsKey(yearY) && !map.containsKey(yearX)) {
+        	// y의 값과 x의 값 모두 존재하지 않을 때
+        	// 무조건 maybe를 출력
+        	if (!map.containsKey(y) && !map.containsKey(x)) {
         		bw.write("maybe\n");
         	}
-        	// 2. Y의 정보만 있다면, Y+1부터 X-1 사이의 최댓값보다 Y가 클 경우 -> maybe
-        	else if (map.containsKey(yearY) && !map.containsKey(yearX)) {
-        		int left = map.get(yearY) + 1;
-        		int right = binarySearch(yearX) - 1;
-        		int maxRain = query(left, right);
+        	
+        	// y의 값만 존재할 때
+        	// (y + 1) ~ (x 직전 값) 의 최대값보다 y의 값이 더 크다면 -> maybe
+        	else if (map.containsKey(y) && !map.containsKey(x)) {
+        		left = map.get(y) + 1;
+        		right = binarySearch(x) - 1;
+        		maxValue = query(left, right);
         		
-        		if (maxRain < indexTree[map.get(yearY) + base - 1]) {
+        		if (maxValue < indexTree[map.get(y) + base - 1]) {
         			bw.write("maybe\n");
         		} else {
         			bw.write("false\n");
         		}
         	}
-        	// 3. X의 정보만 있다면, Y부터 X-1 사이의 최댓값보다 X가 클 경우 -> maybe
-        	else if (!map.containsKey(yearY) && map.containsKey(yearX)) {
-        		int left = binarySearch(yearY);
-        		int right = map.get(yearX) - 1;
-        		int maxRain = query(left, right);
+        	
+        	// x의 값만 존재할 때
+        	// (y 직후 값) ~ (x - 1) 의 최대값보다 x의 값이 더 크다면 -> maybe
+        	else if (!map.containsKey(y) && map.containsKey(x)) {
+        		left = binarySearch(y);
+        		right = map.get(x) - 1;
+        		maxValue = query(left, right);
         		
-        		if (maxRain < indexTree[map.get(yearX) + base - 1]) {
+        		if (maxValue < indexTree[map.get(x) + base - 1]) {
         			bw.write("maybe\n");
         		} else {
         			bw.write("false\n");
         		}
         	}
-        	// 4. X와 Y 정보 모두 있다면, Y+1부터 X-1 사이의 최댓값보다 X가 클 경우
-        	// -> 연속 조건도 참인 경우: true
-        	// -> 연속 조건은 거짓일 경우: maybe
+        	
+        	// y, x 모두 존재할 때
+        	// y >= x를 만족하고, (y + 1) ~ (x - 1) 의 최대값보다 x가 더 크다면 -> maybe
+        	// 중간에 빈 값도 없다면 -> true
         	else {
-        		int left = map.get(yearY) + 1;
-        		int right = map.get(yearX) - 1;
-        		int maxRain = query(left, right);
+        		left = map.get(y) + 1;
+        		right = map.get(x) - 1;
+        		maxValue = query(left, right);
         		
-        		if (maxRain < indexTree[map.get(yearX) + base - 1] && indexTree[map.get(yearY) + base - 1] >= indexTree[map.get(yearX) + base - 1]) {
-        			if ((right + 1) - (left - 1) == yearX - yearY) {
+        		if (indexTree[map.get(y) + base - 1] >= indexTree[map.get(x) + base - 1] && maxValue < indexTree[map.get(x) + base - 1]) {
+        			if (x - y == map.get(x) - map.get(y)) {
         				bw.write("true\n");
         			} else {
         				bw.write("maybe\n");
@@ -99,63 +100,60 @@ public class Main {
         }
         
         bw.flush();
-        br.close();
         bw.close();
+        br.close();
     }
     
-    public static void update(int idx, int value) throws IOException {
+    static void update(int index, int value) {
+    	index = index + base - 1;
+    	indexTree[index] = value;
     	
-    	idx = idx + base - 1;
-    	
-    	indexTree[idx] = value;
-    	
-    	while (idx > 1) {
-    		idx /= 2;
-    		indexTree[idx] = Math.max(indexTree[idx * 2], indexTree[idx * 2 + 1]);
+    	while (index > 1) {
+    		index /= 2;
+    		indexTree[index] = Math.max(indexTree[index * 2], indexTree[index * 2 + 1]);
     	}
     }
     
-    static int query(int left, int right) throws IOException{
+    static int query(int left, int right) {
     	
-        int result = 0;
-        
-        left += base - 1;
-        right += base - 1;
-
-        while(left <= right){
-            if(left % 2 == 1){
-                result = Math.max(result, indexTree[left]);
-                left++;
-            }
-
-            if(right % 2 == 0){
-                result = Math.max(result, indexTree[right]);
-                right--;
-            }
-
-            left /= 2;
-            right /= 2;
-        }
-
-        return result;
+    	left = left + base - 1;
+    	right = right + base - 1;
+    	
+    	int maxValue = 0;
+    	
+    	while (left <= right) {
+    		if (left % 2 != 0) {
+    			maxValue = Math.max(maxValue, indexTree[left]);
+    			left++;
+    		}
+    		
+    		if (right % 2 == 0) {
+    			maxValue = Math.max(maxValue, indexTree[right]);
+    			right--;
+    		}
+    		
+    		left /= 2;
+    		right /= 2;
+    	}
+    	
+    	return maxValue;
     }
     
-    public static int binarySearch(int year) {
+    static int binarySearch(int year) {
+    	
     	int start = 1;
     	int end = n;
     	
     	while (start < end) {
     		int mid = (start + end) / 2;
     		
-    		int midYear = years[mid];
-    		
-    		if (midYear < year) {
+    		if (years[mid] < year) {
     			start = mid + 1;
     		} else {
     			end = mid;
     		}
     	}
     	
-    	return end;
+    	return start;
     }
 }
