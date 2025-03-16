508308d7cae310667e56bae709dca1a814c35017
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-03-16 18:53:31 +0900

    [Gold V] Title: 집합의 표현, Time: 388 ms, Memory: 49068 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,66 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	public static BufferedReader br;
-	public static BufferedWriter bw;
+    
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
 	
+	static int n, m;
 	static int[] parent;
-
-	public static void main(String[] args) throws IOException {
-		
-		br = new BufferedReader(new InputStreamReader(System.in));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		parent = new int[n+1];
-		
-		for (int i = 0; i <= n; i++) {
-			parent[i] = i;
-		}
-		
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int flag = Integer.parseInt(st.nextToken());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			if (flag == 0) {
-				union(a, b);
-			} else {
-				if (find(a) == find(b)) {
-					bw.write("YES\n");
-				} else {
-					bw.write("NO\n");
-				}
-			}
-		}
-		
-		bw.flush();
-		br.close();
-		bw.close();
-	}
 	
-	public static void union(int a, int b) {
-		int a_parent = find(a);
-		int b_parent = find(b);
-		
-		if (a_parent < b_parent) {
-			parent[b_parent] = a_parent;
-		} else if (b_parent < a_parent){
-			parent[a_parent] = b_parent;
-		}
-	}
-	
-	public static int find(int n) {
-		if (parent[n] == n) {
-			return n;
-		} else {
-			return parent[n] = find(parent[n]);
-		}
-	}
+    public static void main(String[] args) throws Exception{
+    	br = new BufferedReader(new InputStreamReader(System.in));
+    	bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	st = new StringTokenizer(br.readLine());
+    	n = Integer.parseInt(st.nextToken());
+    	m = Integer.parseInt(st.nextToken());
+    	
+    	parent = new int[n+1];
+    	for (int i = 0; i <= n; i++) {
+    		parent[i] = i;
+    	}
+    	
+    	int flag, a, b;
+    	for (int i = 0; i < m; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		flag = Integer.parseInt(st.nextToken());
+        	if (flag == 0) {
+        		a = Integer.parseInt(st.nextToken());
+        		b = Integer.parseInt(st.nextToken());
+        		union(a,b);
+        	} else {
+        		a = Integer.parseInt(st.nextToken());
+        		b = Integer.parseInt(st.nextToken());
+        		if (find(a) == find(b)) {
+        			bw.write("YES\n");
+        		} else {
+        			bw.write("NO\n");
+        		}
+        	}
+    	}
+    	
+    	bw.flush();
+    	bw.close();
+    	br.close();
+    }
+    
+    static void union(int a, int b) {
+    	int parentA = find(a);
+    	int parentB = find(b);
+    	
+    	parent[parentB] = parentA;
+    }
+    
+    static int find(int x) {
+    	if (x == parent[x]) {
+    		return x;
+    	}
+    	
+    	return parent[x] = find(parent[x]);
+    }
 }
