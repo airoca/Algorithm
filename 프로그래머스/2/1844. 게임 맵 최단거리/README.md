02cdb5950b55baf035fef978d1d56526ee12c9a9
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-03-18 00:41:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.20 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 55.3 MB, 시간: 9.11 ms
+메모리: 9.39 MB, 시간: 12.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 16일 18:52:41
+2025년 03월 18일 00:41:32
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solution(maps):
+    
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    
+    n = len(maps)
+    m = len(maps[0])
+    
+    queue = deque()
+    queue.append([0,0,1])
+    
+    visited = [[False] * m for _ in range(n)]
+    visited[0][0] = True
+    
+    while queue:
+        x,y,cost = queue.popleft()
+        
+        if x == n-1 and y == m-1:
+            return cost;
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+        
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue;
+        
+            if maps[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                queue.append([nx, ny, cost+1])
+    
+    return -1
