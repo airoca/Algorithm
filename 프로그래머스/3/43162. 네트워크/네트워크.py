ba3217b166e68d94138fcef200dff4d1fac97980
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-05-17 23:13:33 +0900

    [level 3] Title: 네트워크, Time: 0.78 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,14 @@
 def solution(n, computers):
     answer = 0
     visited = [False] * n
-    for s in range(n):
-        if visited[s] == False:
-            dfs(n, s, computers, visited)
+    for i in range(n):
+        if visited[i] == False:
+            dfs(i, visited, computers, n)
             answer += 1
     return answer
-
-def dfs(n, s, computers, visited):
-    visited[s] = True
-    for i in range(n):
-        if computers[s][i] == 1 and visited[i] == False:
-            visited[i] = True
-            dfs(n, i, computers, visited)
-    return True
+    
+def dfs(i, visited, computers, n):
+    visited[i] = True
+    for j in range(n):
+        if computers[i][j] == 1 and visited[j] == False:
+            dfs(j, visited, computers, n)
