076e0d5eaa87bd8ddc9be52179e1b1d18f01b684
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2026-03-07 17:29:56 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 164 ms, Memory: 38164 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,27 +1,19 @@
 from collections import deque
-import sys
-
-input = sys.stdin.readline
-INF = 1000000
 
 start, dest = map(int, input().split())
-costs = [INF] * 100001
-queue = deque()
-queue.append([start, 0])
-costs[start] = 0
+INF = int(1e9)
+dist = [INF] * 100001
+dq = deque()
+dq.append(start)
+dist[start] = 0
 
-while queue:
-    cur, cost = queue.popleft()
-
-    for nxt in [cur+1, cur-1, cur*2]:
-        if nxt < 0 or nxt > 100000:
-            continue
-        if nxt == cur*2 and costs[nxt] > cost:
-            costs[nxt] = cost
-            queue.append([nxt, cost])
-        elif costs[nxt] > cost+1:
-            costs[nxt] = cost + 1
-            queue.append([nxt, cost+1])
-
-
-print(costs[dest])
+while dq:
+    cur = dq.popleft()
+    for nxt, cost in [(cur*2,0),(cur-1,1),(cur+1,1)]:
+        if 0 <= nxt <= 100000 and dist[nxt] > dist[cur]+cost:
+            dist[nxt] = dist[cur]+cost
+            if cost==0:
+                dq.appendleft(nxt)
+            else:
+                dq.append(nxt)
+print(dist[dest])
