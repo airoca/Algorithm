e8695780eeaf52f06f1412c351d29a7e8cfef5e8
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2026-03-18 20:49:31 +0900

    [Gold IV] Title: 여행 가자, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -4,16 +4,15 @@ m = int(input())
 parent = [i for i in range(n+1)]
 
 def find_parent(c):
-    if c == parent[c]:
-        return c
-    else:
-        return find_parent(parent[c])
+    if parent[c] != c:
+        parent[c] = find_parent(parent[c])
+    return parent[c]
 
 def union(c1, c2):
     p1 = find_parent(c1)
     p2 = find_parent(c2)
     if p1 != p2: 
-        parent[p2] = find_parent(p1)
+        parent[p2] = p1
 
 for c1 in range(1, n+1):
     cities = list(map(int, input().split()))
