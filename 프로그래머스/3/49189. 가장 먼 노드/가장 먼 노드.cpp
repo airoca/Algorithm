568507887826ad5ecfd993e25ea530962c5b9b2d
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2026-04-09 20:47:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 10.58 ms, Memory: 14.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> edge) {
+    const int INF = 1e9;
+    int answer = 0;
+    vector<vector<int>> adj(n+1);
+    vector<int> cost(n+1, INF);
+    cost[1] = 0;
+    
+    for (auto e : edge) {
+        adj[e[0]].push_back(e[1]);
+        adj[e[1]].push_back(e[0]);
+    }
+    
+    deque<int> queue;
+    queue.push_back(1);
+    
+    while (!queue.empty()) {
+        int cur = queue.front();
+        queue.pop_front();
+        for (auto nxt : adj[cur]) {
+            if (cost[nxt] == INF) {
+                cost[nxt] = cost[cur] + 1;
+                queue.push_back(nxt);
+            }
+        }
+    }
+    
+    cost[0] = 0;
+    int max = *max_element(cost.begin(), cost.end());
+    answer = count(cost.begin(), cost.end(), max);
+    
+    return answer;
+}
