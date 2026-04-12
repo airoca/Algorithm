c629a153f64f9eafd4626ae480ff2f7e847efb6d
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2026-04-12 13:41:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.71 ms, Memory: 3.98 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,25 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int solution(vector<vector<int> > maps)
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+
+int solution(vector<vector<int>> maps)
 {
-    
+    int answer = 0;
     int n = maps.size();
     int m = maps[0].size();
     
-    int dx[] = {1,-1,0,0};
-    int dy[] = {0,0,1,-1};
-    
     deque<vector<int>> queue;
-    vector<vector<bool>> visited(n, vector<bool>(m, false));
-    
-    visited[0][0] = true;
-    queue.push_back({0, 0, 1});
+    queue.push_back({0,0,1});
     
     while (!queue.empty()) {
         auto cur = queue.front();
         queue.pop_front();
-        
         int x = cur[0];
         int y = cur[1];
         int cost = cur[2];
@@ -34,8 +30,8 @@ int solution(vector<vector<int> > maps)
             
             if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
             
-            if (!visited[nx][ny] && maps[nx][ny] != 0) {
-                visited[nx][ny] = true;
+            if (maps[nx][ny] == 1) {
+                maps[nx][ny] = cost + 1;
                 queue.push_back({nx, ny, cost + 1});
             }
         }
