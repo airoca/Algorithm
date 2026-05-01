af21946f5ce5b3deb1cbdf3dfcb19f7d8c8b98f6
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2026-05-01 18:17:58 +0900

    [level 2] Title: H-Index, Time: 0.12 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    answer = 0
+    citations.sort()
+    length = len(citations)
+    
+    for i in range(length):
+        if citations[i] >= length - i:
+            return length - i
+    
+    return answer
